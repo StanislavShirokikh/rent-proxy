fe20459975d98098464e5f620bcaa0827f5b72d8
StanislavShirokikh <shirokikhstanislav@icloud.com>
2024-03-06 22:51:09 +0700

    Initial commit

---
+++ src/main/java/org/example/rentproxy/RentProxyApplication.java
@@ -0,0 +1,13 @@
+package org.example.rentproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentProxyApplication.class, args);
+	}
+
+}
