2274a0e032d4627a183d1f6edc9eb998f8ca971f
StanislavShirokikh <shirokikhstanislav@icloud.com>
2024-03-19 15:17:56 +0700

    add annotation

--- src/test/java/org/example/rentproxy/repository/DeleteEntityTest.java
+++ src/test/java/org/example/rentproxy/repository/DeleteEntityTest.java
@@ -1,7 +1,9 @@
 package org.example.rentproxy.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class DeleteEntityTest {
     @Test
     public void deletePost() {
