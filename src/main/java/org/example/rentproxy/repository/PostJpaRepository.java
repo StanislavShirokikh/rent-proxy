9e0f08f9bd56aaa0eb8889dec7b8da55da73aa9f
StanislavShirokikh <shirokikhstanislav@icloud.com>
2024-04-01 23:12:01 +0700

    refactor

--- src/main/java/org/example/rentproxy/repository/PostJpaRepository.java
+++ src/main/java/org/example/rentproxy/repository/PostJpaRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-@Repository(value = "postJpaRepository")
+@Repository
 public interface PostJpaRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
 }
