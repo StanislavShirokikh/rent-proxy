6e67858cbe17638facdebee0fdb6087dbe9df88b
StanislavShirokikh <shirokikhstanislav@icloud.com>
2024-04-10 02:03:08 +0700

    сделал user controller(без логики)

---
+++ src/main/java/org/example/rentproxy/controller/UserController.java
@@ -0,0 +1,11 @@
+package org.example.rentproxy.controller;
+
+import org.example.rentproxy.dto.UserDto;
+import org.example.rentproxy.repository.entities.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface UserController {
+    @PostMapping("account/registration")
+    void register(@RequestBody UserDto userDto);
+}
---
+++ src/main/java/org/example/rentproxy/controller/UserControllerImpl.java
@@ -0,0 +1,15 @@
+package org.example.rentproxy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.rentproxy.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserControllerImpl implements UserController{
+
+    @Override
+    public void register(UserDto userDto) {
+
+    }
+}
